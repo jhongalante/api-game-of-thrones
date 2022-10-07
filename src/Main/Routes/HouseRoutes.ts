634746f231c152c9ba3114d5e50b4ce8597e7ee5
Jhonata Galante <jhonata.galante@gmail.com>
2022-10-07 11:13:03 -0300

    feat: created route to find

--- src/Main/Routes/HouseRoutes.ts
+++ src/Main/Routes/HouseRoutes.ts
@@ -1,10 +1,10 @@
-import { ListHouseController } from './../../Presentation/Controllers/House/ListHouseController'
 import { AddHouseController } from './../../Presentation/Controllers/House/AddHouseController'
+import { FindHouseController } from '../../Presentation/Controllers/House/FindHouseController'
 import { Router } from 'express'
 import { container } from 'tsyringe'
 import { adaptRoute } from '../Adapters/Express/ExpressRouteAdapters'
 
 export default (router: Router): void => {
   router.post('/house', adaptRoute(container.resolve(AddHouseController)))
-  router.get('/house', adaptRoute(container.resolve(ListHouseController)))
+  router.get('/house', adaptRoute(container.resolve(FindHouseController)))
 }
