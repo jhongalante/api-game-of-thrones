b65ff9763dad2e1bccd8c5210c75a5da0364c6ef
Jhonata Galante <jhonata.galante@gmail.com>
2022-10-07 11:36:57 -0300

    feat: added authorization at house routes

--- src/Main/Routes/HouseRoutes.ts
+++ src/Main/Routes/HouseRoutes.ts
@@ -1,12 +1,13 @@
-import { DeleteHouseController } from './../../Presentation/Controllers/House/DeleteHouseController'
-import { AddHouseController } from './../../Presentation/Controllers/House/AddHouseController'
-import { FindHouseController } from '../../Presentation/Controllers/House/FindHouseController'
 import { Router } from 'express'
 import { container } from 'tsyringe'
+import { AddHouseController } from '../../Presentation/Controllers/House/AddHouseController'
+import { DeleteHouseController } from '../../Presentation/Controllers/House/DeleteHouseController'
+import { FindHouseController } from '../../Presentation/Controllers/House/FindHouseController'
 import { adaptRoute } from '../Adapters/Express/ExpressRouteAdapters'
+import { Auth } from '../Middlewares/Auth'
 
 export default (router: Router): void => {
-  router.post('/house', adaptRoute(container.resolve(AddHouseController)))
-  router.get('/house', adaptRoute(container.resolve(FindHouseController)))
-  router.delete('/house/:id', adaptRoute(container.resolve(DeleteHouseController)))
+  router.post('/house', Auth, adaptRoute(container.resolve(AddHouseController)))
+  router.get('/house', Auth, adaptRoute(container.resolve(FindHouseController)))
+  router.delete('/house/:id', Auth, adaptRoute(container.resolve(DeleteHouseController)))
 }
