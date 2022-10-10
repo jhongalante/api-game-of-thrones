15a9f803b3dde756232033d4ca91c364395b4faa
Jhonata Galante <jhonata.galante@gmail.com>
2022-10-10 10:02:45 -0300

    refactor: moved uri api to the env file

--- src/Data/UseCases/Lord/FindLordExternal/FindLordExternal.ts
+++ src/Data/UseCases/Lord/FindLordExternal/FindLordExternal.ts
@@ -2,11 +2,11 @@ import { IFindLordExternal } from './../../../../Domain/UseCases/Lord/FindLordEx
 import { singleton } from 'tsyringe'
 import axios from 'axios'
 import { ILordExternal } from '../../../../Domain/Models/Lord'
-
+const API_URI = process.env.API_ICEANDFIRE_URI
 @singleton()
 export class FindLordExternal implements IFindLordExternal {
   async find (name: string): Promise<ILordExternal> {
-    const lord = await axios.get('https://www.anapioficeandfire.com/api/characters/', { params: { name } })
+    const lord = await axios.get(API_URI + '/characters/', { params: { name } })
       .then((response) => {
         const character = response.data[0] as ILordExternal
         return character
